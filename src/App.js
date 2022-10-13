bbfc6478f5f4776c793c3d2db15392576729b1e6
Max Valek <84463102+Max-Valek@users.noreply.github.com>
2022-10-13 10:49:21 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Twitter Clone</h1>
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Sidebar.css';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+            <TwitterIcon />
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button -> tweet */}
+        </div>
+    )
+}
+
+export default Sidebar;
